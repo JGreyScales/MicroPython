93a9c25f5d2ecfdfe3a6ff180329b169b5415659
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-03-09 22:10:27 -0500

    Added comments

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
+//  Variable definement
 let seed = 0
 function reset() {
+    //  defines the blank LEDS (YES IK THERE IS A FUNCTION FOR IT ALREADY)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -9,8 +11,11 @@ function reset() {
         `)
 }
 
+//  Mainloop
 while (true) {
+    //  checks if button A is being pressed
     if (input.buttonIsPressed(Button.A)) {
+        //  creates LED display
         basic.showLeds(`
         . . # . .
         . # . . .
@@ -18,18 +23,23 @@ while (true) {
         . # . . .
         . . # . .
         `)
+        //  checks if button is still being held down
         while (input.buttonIsPressed(Button.A)) {
+            //  begins moving the motor
             servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
             motor.turnLeft(25)
             servos.setServoPosition(servos.ServoPin.P8, Position.HALF_DOWN)
             basic.pause(500)
             servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
         }
+        //  stops the motor and resets the LED
         motor.stop()
         reset()
     }
     
+    //  checks if button B is being pressed
     if (input.buttonIsPressed(Button.B)) {
+        //  creates LED display
         basic.showLeds(`
         . . # . .
         . . . # .
@@ -37,7 +47,9 @@ while (true) {
         . . . # .
         . . # . .
         `)
+        //  checks if button B is still being pressed
         while (input.buttonIsPressed(Button.B)) {
+            //  moves the servo
             servos.setServoPosition(servos.ServoPin.P0, Position.MIDDLE)
             basic.pause(2000)
             servos.setServoPosition(servos.ServoPin.P0, Position.HALF_DOWN)
@@ -46,9 +58,11 @@ while (true) {
             seed += 1
             motor.stop()
         }
+        //  resets the LEDS
         reset()
     }
     
+    //  plays annoying sounds if logo touch sensor is pressed
     if (input.logoIsPressed()) {
         music.playTone(Note.C, 10000)
     }
