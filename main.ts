4b7568af59aa0b9ff54de419bf8e0e1fbf7afbed
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2021-03-09 21:20:36 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,43 +1,4 @@
 let seed = 0
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    //  creates LED screen
-    basic.showLeds(`
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        `)
-    //  moves the motor and the servo
-    while (input.buttonIsPressed(Button.A)) {
-        servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
-        motor.turnLeft(25)
-        servos.setServoPosition(servos.ServoPin.P8, Position.HALF_DOWN)
-        basic.pause(500)
-        servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
-    }
-    motor.turnLeft(0)
-    reset()
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    basic.showLeds(`
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        `)
-    while (input.buttonIsPressed(Button.B)) {
-        servos.setServoPosition(servos.ServoPin.P0, Position.MIDDLE)
-        basic.pause(2000)
-        servos.setServoPosition(servos.ServoPin.P0, Position.HALF_DOWN)
-        basic.pause(500)
-        servos.setServoPosition(servos.ServoPin.P0, Position.MIDDLE)
-        seed += 1
-    }
-    reset()
-})
 function reset() {
     basic.showLeds(`
         . . . . .
@@ -48,3 +9,44 @@ function reset() {
         `)
 }
 
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+        while (input.buttonIsPressed(Button.A)) {
+            servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
+            motor.turnLeft(25)
+            servos.setServoPosition(servos.ServoPin.P8, Position.HALF_DOWN)
+            basic.pause(500)
+            servos.setServoPosition(servos.ServoPin.P8, Position.MIDDLE)
+        }
+        motor.stop()
+        reset()
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+        while (input.buttonIsPressed(Button.B)) {
+            servos.setServoPosition(servos.ServoPin.P0, Position.MIDDLE)
+            basic.pause(2000)
+            servos.setServoPosition(servos.ServoPin.P0, Position.HALF_DOWN)
+            basic.pause(500)
+            servos.setServoPosition(servos.ServoPin.P0, Position.MIDDLE)
+            seed += 1
+            motor.stop()
+        }
+        reset()
+    }
+    
+}
